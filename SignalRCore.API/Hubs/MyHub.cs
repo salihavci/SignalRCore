3c11e4a053e2fb01799a5831508262ddfeac19d4
salihavci <cacabeystjr@gmail.com>
2022-09-23 18:51:56 +0300

    Proje dosyası ekle.

---
+++ SignalRCore.API/Hubs/MyHub.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.SignalR;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace SignalRCore.API.Hubs
+{
+    public class MyHub: Hub
+    {
+        public static List<string> Names { get; set; } = new List<string>();
+        public async Task SendName(string name)
+        {
+            Names.Add(name);
+            await Clients.All.SendAsync("ReceiveName",name).ConfigureAwait(false); //İlk parametre metot ismi, diğer parametreler argümanda gidecek datalar.
+        }
+        public async Task GetNames()
+        {
+            await Clients.All.SendAsync("ReceiveNames",Names).ConfigureAwait(false);
+        }
+    }
+}
